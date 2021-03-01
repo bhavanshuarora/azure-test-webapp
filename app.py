d63528ac405cf814a5d307d3939967008e881016
bhavanshuarora <63899693+bhavanshuarora@users.noreply.github.com>
2021-03-01 13:51:45 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+  
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Bhavanshu Arora webApp test!  <b>hello !!</b>  <img src='https://myimagest.blob.core.windows.net/myclwimage/vimal.jpg'  width='150' height='200' />"
