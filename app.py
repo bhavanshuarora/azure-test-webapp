c1e14c3a07e202f3327467c07dbc5ec5dda36e6e
bhavanshuarora <63899693+bhavanshuarora@users.noreply.github.com>
2021-03-01 14:14:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Bhavanshu Arora webApp test!  <b>hello !!</b>  <img src='https://myimagest.blob.core.windows.net/myclwimage/vimal.jpg'  width='150' height='200' />"
+    return "Bhavanshu Arora webApp test!  <b>hello !!</b>  <img src='https://drive.google.com/file/d/1sPy6a1FNRcyhpWn9_yK_C7l_nvRxEC4M/view?usp=sharing'  width='150' height='200' />"
